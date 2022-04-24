c9b04cbe1d6ae67d66bbe059d1965ec8637b2e86
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-04-24 11:12:45 +0500

    update

---
+++ 03- CPP for Competitive Programming By Luv/01- C++ for Competitive Programming By Luv/tut_1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int main(){
+
+ // cout << "Hello " ; 
+ // cout <<sqrt(16);
+
+// 01- Variable
+ 
+ // int a=2;
+ // int b=2;
+ // int sum=a+b;
+ // cout << sum <<endl;
+
+//02- Data Type 
+
+// char int , float , double,
+//long int , long long int,
+
+// char c='3';
+// int a=2;
+// float b= 2.3;
+// bool d=false;
+// long long q= 1234555;
+
+
+// cout<< c <<" "<< a <<" "<<b<<" "<<d<< endl;
+
+ //  03- Operators:
+
+  // cout<< 15%2<<endl;
+  // int a= 4;
+  // a=a+1;
+  // cout<<a<<endl;
+  // cout<<++a<<endl;
+  
+  // Ascii Value will print 
+
+	// char c ='a';
+	// char b ='c';
+	// cout << (int)c;
+    // cout<<c-b;
+
+    int a;
+    double b;
+    char c;
+    // double b;
+    cin >> a >> b >> c;
+    // cout << a+ b;
+    cout<< a <<" "<< b <<" "<< c << endl;
+
+
+
+
+ return 0;
+}
---
+++ 03- CPP for Competitive Programming By Luv/01- C++ for Competitive Programming By Luv/tut_2.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// #include <math.h>
+// using namespace std;
+// int main(){
+
+//  // cout << 'c' +1 ;
+//  // return 0;
+
+// // Overflow
+
+// 	// int , char, long int, long long int, float, double,
+// 	// -10^9  < int < 10^9
+// 	// -10^12 < long int <10^12
+// 	// -10^18 < long long int< 10^18
+
+// // 1LL // fixed
+
+// 	int a= 100000;
+// 	int b= 100000;
+// 	int c= a*b;
+// 	cout << c << endl;
+// 	int mx= INT_MAX;
+// 	cout << mx << endl;
+
+// }
+
+
+
