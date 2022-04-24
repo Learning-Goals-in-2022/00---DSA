299887a7b8019e28177693ddc9d6df0b5d851ae2
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-04-24 11:20:22 +0500

    update

---
+++ 02- OnGoing CPP Placement DSA Course By Love Babber/Lecture 02- Write Your First Program in C++/C++ Code/Lec02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout << "Hello World" << endl;
+    // return 0;
+
+    // int a = 123;
+    // cout << a << endl;
+    // int size = sizeof(a);
+    // cout << "size of a is " << size << endl;
+
+    double b = 1.23;
+    cout << b << endl;
+    double size = sizeof(b);
+    cout << "size of b is " << size << endl;
+}
