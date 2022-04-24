3b5cc9d06825e5b333c995d7ccaf4839f1a17153
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-04-24 12:39:55 +0500

    update

---
+++ 02- OnGoing CPP Placement DSA Course By Love Babber/Lecture 03- If-Else, While loop & Lots of Patterns (Part-1)/C++ Code/tut3.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a;
+    // cin >> a;
+    // cout << "Your Output is " << a << endl;
+
+    // return 0;
+
+    // int a, b;
+    // cin >> a >> b;
+    // a = cin.get();
+
+    // cout << "Value of a and b is " << a << b << endl;
+    // cout << "the value of a is :" << a << endl;
+
+    // more statements
+
+    // int a;
+    // cout << "Enter the vaue of A" << endl;
+    // cin >> a;
+    // if (a > 0)
+    // {
+    //     cout << "A is +Ve";
+    // }
+    // else
+    // {
+    //     if (a < 0)
+    //     {
+    //         cout << "A is -ve";
+    //     }
+    //     else
+    //     {
+    //         cout << "A is 0";
+    //     }
+    // }
+    // cout << "the vaue of A is " << a << endl;
+
+    // ELse if
+
+    // int a;
+    // cout << "Eenter the value of A" << endl;
+    // cin >> a;
+
+    // if (a > 0)
+    // {
+    //     cout << "A is +ve" << endl;
+    // }
+    // else if (a < 0)
+    // {
+    //     cout << "A is -ve" << endl;
+    // }
+    // else
+    // {
+    //     cout << "A is 0" << endl;
+    // }
+
+    // HomeWork By Ahsan
+
+    // char ch;
+    // cout << "Eenter the value of Ch" << endl;
+    // cin >> ch;
+
+    // if (ch > 'a' && ch <= 'z')
+    // {
+    //     cout << "Lower Case" << endl;
+    // }
+    // else if (ch > 'A' && ch < 'Z')
+    // {
+    //     cout << "Upper case" << endl;
+    // }
+    // else
+    // {
+    //     cout << "This is Number" << endl;
+    // }
+
+    // Another Way
+
+    //  Copy from Youtube Comment
+
+    // char ch;
+    // cout << "Enter the value of ch " << endl;
+    // cin >> ch;
+
+    // if (ch > 96 && ch < 123)
+    // {
+    //     cout << "Lower case";
+    // }
+    // else if (ch > 64 && ch < 91)
+    // {
+    //     cout << "Upper case";
+    // }
+
+    // else if (ch > 47 && ch < 58)
+    // {
+    //     cout << "Numeric";
+    // }
+    // else
+    // {
+    //     cout << "symbol";
+    // }
+
+    // While Loos
+
+    // int n;
+    // cin >> n;
+    // int a = 1;
+    // while (a <= n)
+    // {
+    //     cout << a << endl;
+    //     a = a + 1;
+    // }
+
+    // Print sum of numbers
+
+    // int n;
+    // cin >> n;
+
+    // int sum = 0;
+    // int a = 1;
+    // while (a <= n)
+    // {
+    //     sum = sum + a;
+    //     a = a + 1;
+    // }
+    // cout << "Sum is" << sum << endl;
+
+    // Print Even Number
+
+    // int n;
+    // cin >> n;
+
+    // int sum = 0;
+    // int a = 1;
+    // while (a <= n)
+    // {
+    // sum = sum + a;
+    //     a = a + 2;
+    // }
+    // cout << "Sum is" << a << endl;
+
+    // print 2 paterens
+    // return 0;
+}
