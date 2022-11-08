4b4154dd0012a4117e307cd4fafa58e09fe53ea9
Muhammad Ahsan <muhammadahsan7099@gmail.com>
2022-11-08 19:19:09 +0500

    assignment 01

---
+++ 01 - OnGoing CPP Placement DSA Course By Love Babber/Lecture 42 - OOPs Concepts in C++  Part-1/oop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class hero
+{
+
+    // properties
+    // int health;     // no property then ans is 1
+};
+
+int main()
+{
+
+    // creating an Object
+    hero h1;
+
+    cout << " the size of : " << sizeof(h1) << endl;
+    return 0;
+}
